e81c97b97e06e7f91a75be806ae8b3162bcba9c0
huyhq4 <huyhq4@vng.com.vn>
2020-06-04 13:05:43 +0700

    init

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,11 @@
+import { ManageMenuComponent } from './manage-menu/manage-menu.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { CreateTableComponent } from './create-table/create-table.component';
 const adminRoutes: Routes = [
     {
-        path: "", component: CreateTableComponent
+        path: 'manage-menu', component: ManageMenuComponent
     }
 ];
 
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { CreateTableComponent } from './create-table/create-table.component';
+import { ManageMenuComponent } from './manage-menu/manage-menu.component';
 
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    CreateTableComponent
+    CreateTableComponent,
+    ManageMenuComponent
   ],
   imports: [
     CommonModule,
