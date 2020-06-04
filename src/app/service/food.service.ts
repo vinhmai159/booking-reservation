ed6418f31e85446a9b00ace83812ab846f8661e1
huyhq4 <huyhq4@vng.com.vn>
2020-06-04 13:54:01 +0700

    [Update] change service path

---
+++ src/app/service/food.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AngularFirestore } from '@angular/fire/firestore/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor(private http: HttpClient, private firestore: AngularFirestore) { }
+
+  getFoods(): Observable<any> {
+    return this.firestore.collection('foods').snapshotChanges();
+  }
+  deleteTable(foodId: string){
+    this.firestore.doc('foods/' + foodId).delete();
+  }
+  createTable(food: any){
+    return this.firestore.collection('foods').add(food);
+  }
+  updateTable(food: any){
+    delete food.id;
+    this.firestore.doc('foods/' + food.id).update(food);
+  }
+}
